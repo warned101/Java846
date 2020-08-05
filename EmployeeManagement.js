12ffaef71f6c8a3156cd48480bed4a07656cd8ea
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-05 15:03:56 +0530

    Product module added

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -4,6 +4,10 @@ class Calculator {
   }
   
   public void add(int a, int b) {
-    System.out.println("Sum = " + (a-b));
+    System.out.println("Difference = " + (a-b));
+  }
+  
+  public void prod(int a, int b) {
+    System.out.println("Product = " + (a*b));
   }
 }

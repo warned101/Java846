724aef945c3861ad12b3c4733555d4791a41bbd1
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-05 14:50:27 +0530

    Added diff module

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -2,4 +2,8 @@ class Calculator {
   public void add(int a, int b) {
     System.out.println("Sum = " + (a+b));
   }
+  
+  public void add(int a, int b) {
+    System.out.println("Sum = " + (a-b));
+  }
 }

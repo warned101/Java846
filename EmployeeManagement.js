415b0ea932020784ffb71e46bbad16dbf36a0f88
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-05 15:08:53 +0530

    div module added

--- EmployeeManagement.js
+++ EmployeeManagement.js
@@ -6,4 +6,8 @@ class Calculator {
   public void add(int a, int b) {
     System.out.println("Sum = " + (a-b));
   }
+  
+  public void div(int a, int b) {
+    System.out.println("Quotient = " + (a/b));
+  }
 }
